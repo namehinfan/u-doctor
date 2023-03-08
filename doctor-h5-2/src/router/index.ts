9db029b37099cc90c3057763df80c7477cd61f1b
namehinfan <pd5350488@gmail.com>
2023-03-08 21:53:30 +0800

    用户-布局容器

--- doctor-h5-2/src/router/index.ts
+++ doctor-h5-2/src/router/index.ts
@@ -3,7 +3,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: () => import('@/views/Layout/index.vue') },
+    { path: '/', 
+      component: () => import('@/views/Layout/index.vue'),
+      redirect: '/home',
+      children: [
+        { path: "/home", component: () => import("@/views/Home/index.vue")},
+        {
+          path: "/article",
+          component: () => import("@/views/Article/index.vue"),
+        },
+        { path: "/notify", component: () => import("@/views/Notify/index.vue") },
+        { path: "/user", component: () => import("@/views/User/index.vue") },
+      ]
+    },
     { path: '/login', component:  () => import('@/views/Login/index.vue') }
   ]
 })
---
+++ doctor-h5-2/src/views/User/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  <div class="user-page">user</div>
+  <RouterView />
+  <van-tabbar route>
+  <van-tabbar-item to="/home">首页</van-tabbar-item>
+  <van-tabbar-item to="/article">健康百科</van-tabbar-item>
+  <van-tabbar-item to="/notify">消息中心</van-tabbar-item>
+  <van-tabbar-item to="/user">我的</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<style lang="scss" scoped></style>
